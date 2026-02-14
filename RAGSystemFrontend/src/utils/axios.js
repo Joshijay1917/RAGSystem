d1580dc18f9242da09fbd1964f3bdc565d8628ff
Joshijay1917 <jayjoshi1912007@gmail.com>
2026-02-14 09:18:20 +0530

    Update Backend URL

--- RAGSystemFrontend/src/utils/axios.js
+++ RAGSystemFrontend/src/utils/axios.js
@@ -1,5 +1,7 @@
 import axios from 'axios'
 
+const url = import.meta.VITE_BACKEND_URL || 'http://localhost:3000/api'
+
 export const api = axios.create({
-    baseURL: 'http://localhost:3000/api'
+    baseURL: url
 })
--- RAGSystemFrontend/src/utils/socket.js
+++ RAGSystemFrontend/src/utils/socket.js
@@ -1,3 +1,5 @@
 import { io } from 'socket.io-client'
 
-export const socket = io('http://localhost:3000')
+const url = import.meta.VITE_BACKEND_URL || 'http://localhost:3000/api'
+
+export const socket = io(url)
