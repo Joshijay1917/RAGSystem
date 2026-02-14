e351e1ab62caffbce9640734c26d9048994f06e3
Joshijay1917 <jayjoshi1912007@gmail.com>
2026-02-14 12:20:13 +0530

    Add loader component in sidebar

---
+++ RAGSystemFrontend/src/components/Loader.jsx
@@ -0,0 +1,12 @@
+
+function Loader() {
+    return (
+        <div className="flex items-center justify-center">
+            <div
+                className='h-8 w-8 border-3 rounded-full border-(--color-primary) animate-spin border-t-transparent'
+            />
+        </div>
+    )
+}
+
+export default Loader
--- RAGSystemFrontend/src/components/Sidebar.jsx
+++ RAGSystemFrontend/src/components/Sidebar.jsx
@@ -3,10 +3,11 @@ import Details from '../pages/Details'
 import HomeInfo from './HomeInfo'
 import { useUser } from '../context/UserContext'
 import { useEffect } from 'react'
+import Loader from "./Loader"
 
 function Sidebar() {
     const navigate = useNavigate()
-    const { filesUploaded, file } = useUser()
+    const { filesUploaded, file, loading } = useUser()
 
     useEffect(() => {
         filesUploaded()
@@ -17,6 +18,8 @@ function Sidebar() {
         <div className="bg-zinc-900 text-white w-1/4 p-6 border border-zinc-800">
             <button onClick={() => navigate('/status')} className='bg-blue-600 w-full rounded-lg my-2 py-2'>See Status</button>
             <h2 className="text-xl font-semibold mb-4">Uploaded Files</h2>
+
+            {loading && <Loader />}
 
             <div className="space-y-2">
                 {file && (
